5c6ea86b049c1c392e8f20e0ae0d7aa4cd28e269
progresseo <94923126+progresseo@users.noreply.github.com>
2022-05-03 10:10:39 +0100

    update
    created table

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SQLite;
 
 namespace Habit_Tracker
 {
@@ -7,7 +8,60 @@ namespace Habit_Tracker
         static void Main(string[] args)
         {
 
-            Console.WriteLine("Hello World testing!");
+            string createQuery = @"CREATE TABLE IF NOT EXISTS
+                                [MyTable] (
+                                [Id] INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+                                [Name] NVARCHAR(2048) NULL,
+                                [Gender] NVARCHAR(2048) NULL)";
+            //System.Data.SQLite.SQLiteConnection.CreateFile("test1.db3");
+            using (System.Data.SQLite.SQLiteConnection conn = new System.Data.SQLite.SQLiteConnection("data source=test1.db3"))
+            {
+                using (System.Data.SQLite.SQLiteCommand cmd = new System.Data.SQLite.SQLiteCommand(conn))
+                {
+                    conn.Open();
+                    cmd.CommandText = createQuery;
+                    cmd.ExecuteNonQuery();
+                    
+
+                    //Inputing information
+                    Console.WriteLine("what is your name?");
+                    string name = Console.ReadLine();
+                    Console.WriteLine("What is your gender?");
+                    string gender = Console.ReadLine();
+
+                    cmd.CommandText = "INSERT INTO MyTable(Name,Gender) values(@namevalue,@gendervalue)";
+                    cmd.Parameters.AddWithValue("@namevalue", name);
+                    cmd.Parameters.AddWithValue("@gendervalue", gender);
+                    cmd.ExecuteNonQuery();
+
+
+                    //Deleting the latest entry
+                     cmd.CommandText = "DELETE FROM MyTable WHERE ID=(SELECT MAX(id) FROM MyTable)";
+                     cmd.ExecuteNonQuery();
+
+                    //Deleting a specific entry
+                    Console.WriteLine("What entry do you want to delete?");
+                    int id = Convert.ToInt32(Console.ReadLine());
+                    cmd.CommandText = "DELETE FROM MyTable WHERE Id=@idvalue.ToString()";
+                    cmd.Parameters.AddWithValue("@idvalue", id);
+                    cmd.ExecuteNonQuery();
+
+                    cmd.CommandText = "SELECT * from MyTable";
+                    using (System.Data.SQLite.SQLiteDataReader reader = cmd.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            Console.WriteLine( reader["Name"] + ":" + reader["Gender"]);
+                        }
+                        conn.Close();
+                    }
+
+                }
+
+
+
+
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # Habit_Tracker
- First Project
+ Recording my journey to complete this first project! 
+
+ > I had already installed Visual Studio, GitDesktop, made a github profile. 
+ > Created a new project in VS and comitted it. 
+ > Install SQLite and created a db. Got confused between .db and .sqppro files. This was because I was clicking on save project after creating and editing the database. Saving a project creates a .sqppro. This took me a while to figure out. Just press ctrl+s to save and close database, don't click save project.
+ 
+ > Pasted the code from csharacademy. Got lots of errors! Got very confused. 
+  > Followed youtube tutorial on how to make a SQLite connection. https://www.youtube.com/watch?v=APVit-pynwQ&ab_channel=DevNami
+  Managed to install a Nuget Package. Realised that I have to pull in the SQLite library by adding using at the top. 
+ > Still got the errors. 
+ > Copied code from the tutorial realised the spelling of the class was different. 
+ > Realised I had downloaded a different Nuget package to the Microsoft one. When back and installed that. So now I am using the intended nuget package for this project. 
+
+ >getting error that connection is not opening so going to just follow the youtube tutorial to make the connection instead. Using a System.Data.SQLite nuget package
+ >Managed to add entry to  the database
+ getting errors when trying to delete a specific entry
+ >https://www.youtube.com/watch?v=_JJwHXZcE4Y
+
